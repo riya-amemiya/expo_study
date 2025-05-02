f0f9794accad86ac7ca25617a31e1165dffc739e
Riya Amemiya <riyaamemiya@gmail.com>
2025-05-02 23:49:08 +0900

    chore: remove unused components and files to clean up the project structure

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+// biome-ignore lint/style/noDefaultExport: <explanation>
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
