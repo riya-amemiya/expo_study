45b2919a92be9a2571a86aa5b53a935bdbc8437d
Yuta Nishi <riyaamemiya@gmail.com>
2025-06-02 11:09:14 +0900

    add tamagui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
+import { defaultConfig } from "@tamagui/config/v4";
 import { Stack } from "expo-router";
+import { createTamagui, TamaguiProvider } from "tamagui";
+
+const config = createTamagui(defaultConfig);
 
 // biome-ignore lint/style/noDefaultExport: <explanation>
 export default function RootLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <TamaguiProvider config={config}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </TamaguiProvider>
   );
 }
